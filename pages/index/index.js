a92fbb4e7364c86ef3a1bc5cd6ae6aae99ccc68f
qinliangjie <15995781344@163.com>
2019-03-01 18:37:21 +0800

    暂时本地化数据
    暂时本地化数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,51 @@
-var information = require('../../data/information.js');
-var work = require('../../data/work.js');
+// var information = require('../../data/information.js');
+// var work = require('../../data/work.js');
 Page({
   /**
    * 页面的初始数据
    */
   data: {
       imgmode:"aspectFill",
-      name:"王洁",
-      post:"前端开发工程师",
-      myimg:"../../img/imgs.png"
+    informatin: {
+      "name": "覃良洁",
+      "post": "3年前端开发",
+      "sex": "女",
+      "age": "25",
+      "alive": "江苏省苏州市",
+      "school": "东华理工大学",
+      "major": "计算机科学与技术",
+      "schoolstart": "2012/09",
+      "schoolend": "2016/06",
+      "education": "本科",
+      "skill": "还行吧还行吧还行吧还行吧还行吧还行吧还行吧还行吧还行吧还行吧还行吧",
+      "hobbies": "",
+      "evaluation": "还行吧还行吧还行吧还行吧还行吧还行吧还行吧还行吧还行吧还行吧还行吧"
+    },
+    workList: [{
+      "id": 1,
+      "startTime": "2018/06",
+      "endTime": "至今",
+      "company": "苏州爱洛克信息技术有限公司",
+      "post": "web前端开发"
+    }, {
+      "id": 2,
+      "startTime": "2017/06",
+      "endTime": "2018/06",
+      "company": "苏州金瑞阳信息技术有限公司",
+      "post": "web前端开发"
+    }, {
+      "id": 3,
+      "startTime": "2016/06",
+      "endTime": "2017/06",
+      "company": "苏州云极客信息技术有限公司",
+      "post": "web前端开发"
+    }, {
+      "id": 4,
+      "startTime": "2015/06",
+      "endTime": "2016/06",
+      "company": "苏州云购信息技术有限公司",
+      "post": "web前端开发实习生"
+    }]
   },
 
   /**
